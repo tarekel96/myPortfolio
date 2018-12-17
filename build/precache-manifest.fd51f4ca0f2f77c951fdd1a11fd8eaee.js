9ef052c2fd2014db0d1ca566a836c1f811658db7
Tarek ElHajjaoui <tarekel96@gmail.com>
2018-12-16 22:38:25 -0800

    did npm build and added build folder, now will try to update Heroku

---
+++ build/precache-manifest.fd51f4ca0f2f77c951fdd1a11fd8eaee.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "95c4c480dd246169e541349c43f6f806",
+    "url": "/static/media/toesntictacs.95c4c480.png"
+  },
+  {
+    "revision": "9604db5ad07975220eab6837d40b65ff",
+    "url": "/static/media/socialSportsMeetup.9604db5a.png"
+  },
+  {
+    "revision": "181cf08f5b7bced3c35f73ff58a9b402",
+    "url": "/static/media/ombdMovie.181cf08f.png"
+  },
+  {
+    "revision": "7b36ec0aca8046fd1533331b5829dbb4",
+    "url": "/static/media/jwt-mern.7b36ec0a.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f1d784449e196a7ae046",
+    "url": "/static/js/main.f1d78444.chunk.js"
+  },
+  {
+    "revision": "ecb4d34034b97a36ec48",
+    "url": "/static/js/1.ecb4d340.chunk.js"
+  },
+  {
+    "revision": "f1d784449e196a7ae046",
+    "url": "/static/css/main.261fa19c.chunk.css"
+  },
+  {
+    "revision": "5f5a02d30a8fbc3c80c63aca5bfe909a",
+    "url": "/index.html"
+  }
+];
