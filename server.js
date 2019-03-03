d81ebb4a507c966cf21a165fba076d4411b2d7bf
Tarek ElHajjaoui <tarekel96@gmail.com>
2019-03-02 22:14:21 -0800

    reconfigured the app layout to put all of react files under client dir and then created an express backend that is able to send get request and has its own package json with custom scripts started working on form files in the react side by adding state empty values and onchange and onsubmit handler functions

--- server.js
+++ server.js
@@ -1,17 +1,45 @@
+//server.js
 const express = require("express");
 const path = require("path");
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3001;
 const app = express();
-// app.use(favicon(__dirname + "/build/favicon.ico"));
+const router = require("./api");
+const bodyParser = require("body-parser"); // Parses incoming request bodies in a middleware before your handlers, available under the req.body property.
+
 // the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, "build")));
-app.get("/ping", function(req, res) {
-  return res.send("pong");
+// app.use(express.static(__dirname));
+// app.use(express.static(path.join(__dirname, "build")));
+
+// enables bodyParser middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// express serves the public folder as static
+app.use(express.static("public"));
+
+// testing with Postman
+app.get("/portfolio", (req, res) => {
+  res.json({
+    test: "the route has hit"
+  });
 });
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+
+app.use("/api", router);
+
+// Send every request to the React app
+// Define any API routes before this runs
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
-app.listen(port, function() {
-  console.log(`Server is running on Port: ${port}`);
+
+// app.get("/", function(req, res) {
+//   res.sendFile(path.join(__dirname, "build", "index.html"));
+// });
+app.listen(port, () => {
+  console.log(`The API server is now running on ${port}`);
 });
